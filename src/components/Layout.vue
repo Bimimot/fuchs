53cf10f23cb55fb830e292997084d049c78ab616
Stepan Popov <s_popov2@wargaming.net>
2025-02-24 18:15:05 +0100

    [Fuchs] add transition

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="wrapper">
-    <Avatar />
-    <Header />
-    <slot />
-  </div>
+  <Transition name="fade">
+    <div class="wrapper" v-if="isShow">
+      <Avatar />
+      <Header />
+    </div>
+  </Transition>
 
   <BackgroundImage :src="BackgroundSrc" is-shadow />
 </template>
 
 <script lang="ts" setup>
+import { onMounted, ref } from 'vue';
 import BackgroundImage from './BackgroundImage.vue';
 import BackgroundSrc from '../assets/images/bg.jpg';
 import Avatar from './Avatar.vue';
 import Header from './Header.vue';
+
+const isShow = ref(false);
+
+onMounted(() => {
+  setTimeout(() => {
+    isShow.value = true;
+  }, 200);
+});
 </script>
 
 <style lang="scss" scoped>
